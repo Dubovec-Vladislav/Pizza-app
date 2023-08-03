5135b68bb4039f2e27623bfb2162d85005370944
Dubovec-Vladislav <dubovecvladislav0@gmail.com>
2023-08-03 09:40:28 +0300

    Creating loading for pizzas

--- src/components/Main/PizzaField/PizzaField.tsx
+++ src/components/Main/PizzaField/PizzaField.tsx
@@ -1,7 +1,7 @@
 import React, { FC, useEffect, useState } from 'react'
 import style from './PizzaField.module.scss'
 import BlockTitle from '../../UI/BlockTitle/BlockTitle'
-import pizzasData from '../../../assets/PizzasData/pizzas.json'
+// import pizzasData from '../../../assets/PizzasData/pizzas.json'
 import PizzaItem from './PizzaItem'
 import Skeleton from '../../UI/Skeleton/Skeleton'
 
@@ -16,11 +16,15 @@ interface Pizza {
 
 const PizzaField: FC = (props) => {
   let [items, setItems] = useState<Pizza[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetch('https://64ca3494b2980cec85c315c6.mockapi.io/items')
       .then((res) => res.json())
-      .then((arr) => setItems(arr))
+      .then((arr) => {
+        setItems(arr)
+        setIsLoading(false);
+      })
   }, []);
 
   return (
@@ -28,16 +32,17 @@ const PizzaField: FC = (props) => {
       <div className={style.title}><BlockTitle text={'Все пиццы'} /></div>
       <div className={style.body}>
         {
-          items.map((pizza) => (
-            <PizzaItem
-              key={pizza.id}
-              imageUrl={pizza.imageUrl}
-              name={pizza.name}
-              types={pizza.types}
-              sizes={pizza.sizes}
-              price={pizza.price} />
-            // <Skeleton />
-          ))
+          isLoading
+            ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+            : items.map((pizza) => (
+              <PizzaItem
+                key={pizza.id}
+                imageUrl={pizza.imageUrl}
+                name={pizza.name}
+                types={pizza.types}
+                sizes={pizza.sizes}
+                price={pizza.price} />
+            ))
         }
       </div>
     </section>
