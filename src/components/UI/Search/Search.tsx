98fa300cf222026f070776202250a3894450ec71
Dubovec-Vladislav <dubovecvladislav0@gmail.com>
2023-08-05 16:42:05 +0300

    Creating auto focus after clear click in Search component

--- src/components/UI/Search/Search.tsx
+++ src/components/UI/Search/Search.tsx
@@ -1,19 +1,28 @@
-import React, { FC, useContext } from 'react'
+import React, { FC, useContext, useRef } from 'react'
 import style from './Search.module.scss'
 import { SearchContext } from '../../../App'
 
 const Search: FC = () => {
   const { searchValue, setSearchValue } = useContext(SearchContext)!;
+  const inputRef = useRef<HTMLInputElement>(null);
+  console.log(inputRef);
+
+  const handleClearClick = () => {
+    setSearchValue('');
+    inputRef.current?.focus();
+  }
 
   return (
     <div className={style.body}>
-      <input className={style.search}
+      <input className={style.input}
+        ref={inputRef}
         value={searchValue}
         placeholder='Поиск пиццы...'
         onChange={(e) => setSearchValue(e.target.value)}
       ></input>
-      {searchValue ? <span className={style.cross} onClick={() => setSearchValue('')}></span> : ''}
-    </div>
+      {searchValue ? <span className={style.cross} onClick={handleClearClick}></span> : ''
+      }
+    </div >
   );
 };
 
