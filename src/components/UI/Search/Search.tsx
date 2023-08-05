cfa429ba9b740170570300cd5acce45722dde2ed
Dubovec-Vladislav <dubovecvladislav0@gmail.com>
2023-08-05 17:50:34 +0300

    Creatig debounce for search

--- src/components/UI/Search/Search.tsx
+++ src/components/UI/Search/Search.tsx
@@ -1,13 +1,25 @@
-import React, { FC, useContext, useRef } from 'react'
+import React, { ChangeEvent, FC, useCallback, useContext, useRef, useState } from 'react'
 import style from './Search.module.scss'
 import { SearchContext } from '../../../App'
+import debounce from 'lodash.debounce'
+
 
 const Search: FC = () => {
-  const { searchValue, setSearchValue } = useContext(SearchContext)!;
+  const [value, setValue] = useState('')
+  const { setSearchValue } = useContext(SearchContext)!;
   const inputRef = useRef<HTMLInputElement>(null);
-  console.log(inputRef);
+  
+  const updateSearchValue = useCallback(debounce((string) => {
+    setSearchValue(string);
+  }, 250), []);
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+    updateSearchValue(e.target.value);
+  }
 
   const handleClearClick = () => {
+    setValue('');
     setSearchValue('');
     inputRef.current?.focus();
   }
@@ -16,11 +28,11 @@ const Search: FC = () => {
     <div className={style.body}>
       <input className={style.input}
         ref={inputRef}
-        value={searchValue}
+        value={value}
         placeholder='Поиск пиццы...'
-        onChange={(e) => setSearchValue(e.target.value)}
+        onChange={handleInputChange}
       ></input>
-      {searchValue ? <span className={style.cross} onClick={handleClearClick}></span> : ''
+      {value ? <span className={style.cross} onClick={handleClearClick}></span> : ''
       }
     </div >
   );
