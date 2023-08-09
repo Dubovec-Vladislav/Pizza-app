f378c43df72386a0da5bb37eeb0ed4b8af1790a5
Dubovec-Vladislav <dubovecvladislav0@gmail.com>
2023-08-09 12:26:24 +0300

    Updating basket in header styles

--- src/components/Basket/EmptyBasket/EmptyBasket.tsx
+++ src/components/Basket/EmptyBasket/EmptyBasket.tsx
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom'
 
 const EmptyBasket: FC = (props) => {
   return (
-    <>
+    <div className={style.body}>
       <div className={style.title}>Корзина пустая <span>&#128533;</span></div>
       <div className={style.subTitle}>
         Вероятней всего, вы не заказывали ещё пиццу. Для того, чтобы заказать пиццу, перейди на главную страницу.
       </div>
       <Link to='/' className={style.img}><img src="/img/UI/empty-basket.svg" alt="empty-basket" /></Link>
-    </>
+    </div>
   );
 };
 
