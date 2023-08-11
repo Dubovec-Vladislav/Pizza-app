c6bfe72f7a36b4e9220e01f73659651b63d6a5ee
Dubovec-Vladislav <dubovecvladislav0@gmail.com>
2023-08-11 19:06:02 +0300

    Updating error logic in full pizza component 2

--- src/components/FullPizza/FullPizza.tsx
+++ src/components/FullPizza/FullPizza.tsx
@@ -1,9 +1,10 @@
 import React, { FC, useEffect } from 'react'
 import style from './FullPizza.module.scss'
-import { fetchPizza, selectPizza } from '../../assets/redux/slices/pizzasSlice'
+import { fetchPizza, selectPizza, selectStatus } from '../../assets/redux/slices/pizzasSlice'
 import { useAppDispatch, useAppSelector } from '../../assets/ts/hooks'
-import { useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom'
 import PizzaItem from '../Main/PizzaField/PizzaItem'
+import Skeleton from '../UI/Skeleton/Skeleton'
 
 
 const FullPizza: FC = () => {
@@ -15,26 +16,24 @@ const FullPizza: FC = () => {
   }, [params.id, dispatch])
 
   const pizza = useAppSelector(selectPizza);
-  console.log(pizza);
-
-  if (!pizza) {
-    return <div>Загрузка</div>
-  }
+  const status = useAppSelector(selectStatus);
 
   return (
     <section className={style.block}>
       <div className={style.body}>
-        {pizza.id ?
-          <PizzaItem
-            key={pizza.id}
-            id={pizza.id || ''}
-            imageUrl={pizza.imageUrl || ''}
-            name={pizza.name || ''}
-            types={pizza.types || []}
-            sizes={pizza.sizes || []}
-            prices={pizza.prices || []}
-          />
-          : <div>Упс... видимо такой пиццы нет</div>
+        {status === 'loading'
+          ? [...new Array(1)].map((_, index) => <Skeleton key={index} />)
+          : status === 'success'
+            ? <PizzaItem
+              key={pizza.id}
+              id={pizza.id || ''}
+              imageUrl={pizza.imageUrl || ''}
+              name={pizza.name || ''}
+              types={pizza.types || []}
+              sizes={pizza.sizes || []}
+              prices={pizza.prices || []}
+            />
+            : <div>Упс... видимо такой пиццы нет</div>
         }
       </div>
     </section>
