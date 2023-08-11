0721a5e4e431b0fea6cb6efba1fefc99ca93db68
Dubovec-Vladislav <dubovecvladislav0@gmail.com>
2023-08-11 18:43:32 +0300

    Import Pizza Item in Full Pizza component

--- src/components/FullPizza/FullPizza.tsx
+++ src/components/FullPizza/FullPizza.tsx
@@ -3,24 +3,29 @@ import style from './FullPizza.module.scss'
 import { fetchPizza, selectPizza } from '../../assets/redux/slices/pizzasSlice'
 import { useAppDispatch, useAppSelector } from '../../assets/ts/hooks'
 import { useParams } from 'react-router-dom';
+import PizzaItem from '../Main/PizzaField/PizzaItem'
 
 
 const FullPizza: FC = () => {
   const dispatch = useAppDispatch();
-  const params = useParams();
+  const params = useParams<{ id: string }>();
 
   useEffect(() => {
     dispatch(fetchPizza(params.id!));
   }, [params.id, dispatch])
 
   const pizza = useAppSelector(selectPizza);
-  console.log(pizza);
+  // console.log(pizza);
+
+  if (!pizza) {
+    return <div>Загрузка</div>
+  }
 
   return (
     <section className={style.block}>
       <div className={style.body}>
-        {pizza.name}
-        <div><img src={pizza.imageUrl} alt="" /></div>
+        {/* {pizza.name}
+        <div className={style.img}><img src={pizza.imageUrl} alt="" /></div>
         <div>Types</div>
         <ul>
           {
@@ -32,7 +37,19 @@ const FullPizza: FC = () => {
           {
             pizza.sizes?.map((size, index) => <li key={index}>{size}</li>)
           }
-        </ul>
+        </ul> */}
+        {pizza ?
+          <PizzaItem
+            key={pizza.id}
+            id={pizza.id || ''}
+            imageUrl={pizza.imageUrl || ''}
+            name={pizza.name || ''}
+            types={pizza.types || []}
+            sizes={pizza.sizes || []}
+            prices={pizza.prices || []}
+          />
+          : <>no</>
+        }
       </div>
     </section>
   );
